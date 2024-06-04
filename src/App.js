08ef900d9c54a883c01a48713b5c7479b7ae22c3
Saravanan M <sarvan.m20@gmail.com>
2024-06-04 10:01:24 +0530

    ProffesionalContainer component updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ProffesionalContainer from "./components/ProffesionalContainer";
 import "./styles/App.scss";
 
 function App() {
   return <div className="App">
     <Header />
+    <ProffesionalContainer />
   </div>;
 }
 
---
+++ src/components/ProffesionalContainer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../styles/ProffesionalContainer.scss";
+import certificate from "../images/cerProgram.svg";
+import star from "../images/star.svg";
+import tick from "../images/tick.svg";
+
+const ProffesionalContainer = () => {
+  const pointersData = [
+    {
+      id: "0",
+      text: "India’s No.1 Online Makeup Course",
+    },
+    {
+      id: "1",
+      text: "Learn by LIVE Do-it-Together Classes",
+    },
+    {
+      id: "2",
+      text: "Unlimited Practise Session to master skills",
+    },
+  ];
+  
+  return (
+    <div className="proffesional-container">
+      <div className="proffesional-text">
+        Proffesional Online <br /> Makeup Course
+      </div>
+      <div className="certificate-container">
+        <div className="certificate-img">
+          <img src={certificate} alt="" />
+        </div>
+        <div className="rating-container">
+          <div className="rating-img">
+            <img src={star} alt="" />
+          </div>
+          <div className="rating-text">Rated 4.85/5</div>
+        </div>
+      </div>
+      <div className="pointers">
+        {pointersData.map((e, i) => {
+          return (
+            <div className="each-pointer" key={e.id}>
+              <div className="pointer-img">
+                <img src={tick} alt="" />
+              </div>
+              <div className="pointer-text">{e.text}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProffesionalContainer;
